956a16d56921cb3662ea94bb886e6bdd02792c56
krishnasurya1110 <krishnasurya1110@gmail.com>
2025-07-27 21:13:29 -0400

    update passing env var

--- gold_layer/bq_data_for_chatbot.py
+++ gold_layer/bq_data_for_chatbot.py
@@ -1,12 +1,17 @@
+import os
 from google.cloud import bigquery
 
 def run_bigquery_sql():
+    # Access environment variables
+    gcp_project_id = os.getenv('GCP_PROJECT_ID')
+    bq_dataset = os.getenv('BQ_DATASET')
+
     # Initialize the BigQuery client
     client = bigquery.Client()
 
     # Hourly data table for BigQuery
-    sql_hourly_data = """
-        CREATE OR REPLACE TABLE `${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.hourly_data` AS
+    sql_hourly_data = f"""
+        CREATE OR REPLACE TABLE `{gcp_project_id}.{bq_dataset}.hourly_data` AS
         WITH relevant_cols_data AS (
             SELECT
                 PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', transit_timestamp) AS transit_timestamp,
@@ -19,7 +24,7 @@ def run_bigquery_sql():
                 CAST(transfers AS FLOAT64) AS transfers,
                 latitude,
                 longitude
-            FROM `${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.nyc`
+            FROM `{gcp_project_id}.{bq_dataset}.nyc`
         )
         SELECT
             transit_timestamp,
@@ -41,13 +46,13 @@ def run_bigquery_sql():
     # Execute the query
     hourly_data_query_job = client.query(sql_hourly_data)
     hourly_data_query_job.result()
-    print("Hourly data has been written to the table: ${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.hourly_data")
+    print(f"Hourly data has been written to the table: {gcp_project_id}.{bq_dataset}.hourly_data")
 
     # Daily data table for BigQuery
-    sql_daily_data = """
-        CREATE OR REPLACE TABLE `${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.daily_data` AS
+    sql_daily_data = f"""
+        CREATE OR REPLACE TABLE `{gcp_project_id}.{bq_dataset}.daily_data` AS
         WITH data_grouped_by_date AS (
-            SELECT 
+            SELECT
                 DATE(transit_timestamp) AS transit_date,
                 borough,
                 station_complex,
@@ -58,26 +63,28 @@ def run_bigquery_sql():
                 CAST(transfers AS FLOAT64) AS transfers,
                 latitude,
                 longitude
-            FROM `${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.nyc`
+            FROM `{gcp_project_id}.{bq_dataset}.nyc`
         )
-        SELECT 
+        SELECT
             transit_date,
             borough,
             station_complex,
             transit_mode,
             payment_method,
             SUM(CAST(ridership AS INT64)) AS ridership,
-            SUM(CAST(transfer AS INT64)) AS transfer
+            SUM(CAST(transfers AS INT64)) AS transfer
         FROM data_grouped_by_date
-        GROUP BY 
+        GROUP BY
             transit_date,
             borough,
             station_complex,
             transit_mode,
             payment_method;
-        """
+    """
 
     # Execute the query
     daily_data_query_job = client.query(sql_daily_data)
     daily_data_query_job.result()
-    print("Daily data has been written to the table: ${{ secrets.GCP_PROJECT_ID }}.${{ secrets.BQ_DATASET }}.daily_data")
+    print(f"Daily data has been written to the table: {gcp_project_id}.{bq_dataset}.daily_data")
+
+run_bigquery_sql()
